0133e6243839971a4cf9ccc54c5b9a6d7af44ce6
Sanjeev S <95563629+PhoenixA380@users.noreply.github.com>
2023-09-25 19:07:09 +0530

    withswings.java

--- hangmanwithswings.java
+++ hangmanwithswings.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 
-public class hangmanwithswings {
+public class withswings {
     private final JFrame frame;
     private final JPanel keyboardPanel;
     private final JLabel wordLabel;
@@ -116,7 +116,7 @@ public class hangmanwithswings {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new hangmanwithswings();
+                new withswings();
             }
         });
     }
